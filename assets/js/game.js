5433164d6fa24cb3867ed26563c18ae80329de3b
Andrew Durham <andrewdurham1094@gmail.com>
2021-08-04 12:27:12 -0600

    fixed empty name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -150,9 +150,16 @@ var shop = function() {
     }
 };
 
+//function to get name
+var getPlayerName = function() {
+    var name = "";
+    while(name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+}
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
