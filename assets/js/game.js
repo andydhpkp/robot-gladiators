92fa15951712506ed1605f134e640f0967c24453
Andrew Durham <andrewdurham1094@gmail.com>
2021-08-04 11:34:30 -0600

    cleaned up switch statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -126,29 +126,12 @@ var shop = function() {
     switch(shopOptionPrompt) {
         case "REFILL":
         case "refill":
-            if (playerInfo.money >= 7) {
-                window.alert("Refilling player's health by 20 for 7 dollars.");
-                //increase health and decrease money
-                playerInfo.health = playerInfo.health + 20;
-                playerInfo.money = playerInfo.money - 7;
-            } else {
-                window.alert("You don't have enough money!");
-            }
-            
+            playerInfo.refillHealth();
             break;
         
         case "UPGRADE":
         case "upgrade":
-            if(playerInfo.money >= 7) {
-                window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-                //increase attack and decrease money
-                playerInfo.attack = playerInfo.attack + 6;
-                playerInfo.money = playerInfo.money - 7;
-            } else {
-                window.alert("You don't have enough money");
-            }
- 
+            playerInfo.upgradeAttack();
             break;
 
         case "LEAVE":
@@ -177,6 +160,24 @@ var playerInfo = {
         this.health = 100;
         this.money = 10;
         this.attack = 10;
+    },
+    refillHealth: function() {
+        if (this.money >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars");
+            this.health += 20;
+            this.money -= 7;
+        } else {
+            window.alert("You don't have enough money");
+        }
+    },
+    upgradeAttack: function() {
+        if (this.money >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+            this.attack += 6;
+            this.money -= 7;
+        } else {
+            window.alert("You don't have enough money!");
+        }
     }
 };
 
